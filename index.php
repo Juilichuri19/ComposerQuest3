d04795a504e0bc142beb01d0aaf482bdf9680be8
Juili <juilichuri19@gmail.com>
2021-12-14 21:57:04 +0100

    created index file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'vendor/autoload.php';
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+//$output = $bessie->say();
+//echo $output;
+echo $bessie;
+
+$bessie->setTongue('U');
+echo $bessie;
